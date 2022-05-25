f6340ed18252b6edf32e137e6a5591ebf15820a8
dayorolands <dayorolands60@gmail.com>
2022-05-25 08:14:17 +0100

    Making my tracking service a foreground service

--- app/src/main/java/com/dayo/runningapp/services/TrackingService.kt
+++ app/src/main/java/com/dayo/runningapp/services/TrackingService.kt
@@ -1,19 +1,56 @@
 package com.dayo.runningapp.services
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.NotificationManager.IMPORTANCE_LOW
+import android.app.PendingIntent
+import android.app.PendingIntent.FLAG_UPDATE_CURRENT
+import android.content.Context
 import android.content.Intent
+import android.os.Build
+import androidx.annotation.RequiresApi
+import androidx.core.app.NotificationCompat
 import androidx.lifecycle.LifecycleService
+import androidx.lifecycle.MutableLiveData
 import com.dayo.runningapp.Other.Constants.ACTION_PAUSE_SERVICE
+import com.dayo.runningapp.Other.Constants.ACTION_SHOW_TRACKING_FRAGMENT
 import com.dayo.runningapp.Other.Constants.ACTION_START_OR_RESUME_SERVICE
 import com.dayo.runningapp.Other.Constants.ACTION_STOP_SERVICE
+import com.dayo.runningapp.Other.Constants.NOTIFICATION_CHANNEL_ID
+import com.dayo.runningapp.Other.Constants.NOTIFICATION_CHANNEL_NAME
+import com.dayo.runningapp.Other.Constants.NOTIFICATION_ID
+import com.dayo.runningapp.R
+import com.dayo.runningapp.ui.MainActivity
+import com.google.android.gms.maps.model.LatLng
 import timber.log.Timber
 
+typealias Polyline = MutableList<LatLng>
+typealias Polylines = MutableList<Polyline>
+
 class TrackingService : LifecycleService() {
+
+    var isFirstRun = true
+
+    companion object{
+        val isTracking = MutableLiveData<Boolean>()
+        val pathPoints = MutableLiveData<Polylines>()
+    }
+
+    private fun postInitialValues(){
+        isTracking.postValue(false)
+        pathPoints.postValue(mutableListOf())
+    }
 
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
         intent?.let {
             when(it.action){
                 ACTION_START_OR_RESUME_SERVICE -> {
-                    Timber.d("Started or resumed service")
+                    if(isFirstRun){
+                        startForegroundService()
+                        isFirstRun = false
+                    } else{
+                        Timber.d("Resuming service")
+                    }
                 }
                 ACTION_PAUSE_SERVICE -> {
                     Timber.d("Paused service")
@@ -25,5 +62,47 @@ class TrackingService : LifecycleService() {
         }
 
         return super.onStartCommand(intent, flags, startId)
+    }
+
+    private fun startForegroundService(){
+        val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
+            createNotificationChannel(notificationManager)
+        }
+
+        val notificationBuilder = NotificationCompat.Builder(this, NOTIFICATION_CHANNEL_ID)
+            .setAutoCancel(false)
+            .setOngoing(true)
+            .setSmallIcon(R.drawable.ic_directions_run_black_24dp)
+            .setContentTitle("Running App")
+            .setContentText("00:00:00")
+            .setContentIntent(getMainActivityPendingIntent())
+
+        startForeground(NOTIFICATION_ID, notificationBuilder.build())
+    }
+
+    private fun getMainActivityPendingIntent() = PendingIntent.getActivity(
+        this,
+        0,
+        Intent(this, MainActivity::class.java).also {
+            it.action = ACTION_SHOW_TRACKING_FRAGMENT
+        },
+        FLAG_UPDATE_CURRENT
+    )
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    private fun createNotificationChannel(notificationManager: NotificationManager){
+        val channel = NotificationChannel(
+            NOTIFICATION_CHANNEL_ID,
+            NOTIFICATION_CHANNEL_NAME,
+            IMPORTANCE_LOW
+        )
+        notificationManager.createNotificationChannel(channel)
+    }
+
+    private fun addEmptyPolyline() = pathPoints.value?.apply {
+        add(mutableListOf())
+        pathPoints.postValue(this)
     }
 }
--- app/src/main/java/com/dayo/runningapp/ui/MainActivity.kt
+++ app/src/main/java/com/dayo/runningapp/ui/MainActivity.kt
@@ -1,11 +1,14 @@
 package com.dayo.runningapp.ui
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.View
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.ui.setupWithNavController
+import com.dayo.runningapp.Other.Constants
+import com.dayo.runningapp.Other.Constants.ACTION_SHOW_TRACKING_FRAGMENT
 import com.dayo.runningapp.R
 import com.dayo.runningapp.db.RunDAO
 import dagger.hilt.android.AndroidEntryPoint
@@ -19,6 +22,8 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        navigateToTrackingFragmentIfNeeded(intent)
+
         setSupportActionBar(toolbar)
         bottomNavigationView.setupWithNavController(navHostFragment.findNavController())
 
@@ -30,5 +35,16 @@ class MainActivity : AppCompatActivity() {
                     else -> bottomNavigationView.visibility = View.INVISIBLE
                 }
             }
+    }
+
+    override fun onNewIntent(intent: Intent?) {
+        super.onNewIntent(intent)
+        navigateToTrackingFragmentIfNeeded(intent)
+    }
+
+    private fun navigateToTrackingFragmentIfNeeded(intent: Intent?){
+        if(intent?.action == ACTION_SHOW_TRACKING_FRAGMENT){
+            navHostFragment.findNavController().navigate(R.id.action_global_trackingFragment)
+        }
     }
 }
