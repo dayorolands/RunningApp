56b6674efce8f680a8ef0806763cf8a6ae4b2500
dayorolands <dayorolands60@gmail.com>
2022-05-30 21:08:16 +0100

    Tracking user location in the background

--- app/src/main/java/com/dayo/runningapp/services/TrackingService.kt
+++ app/src/main/java/com/dayo/runningapp/services/TrackingService.kt
@@ -7,20 +7,31 @@ import android.app.PendingIntent
 import android.app.PendingIntent.FLAG_UPDATE_CURRENT
 import android.content.Context
 import android.content.Intent
+import android.location.Location
+import android.location.LocationRequest
 import android.os.Build
+import android.os.Looper
 import androidx.annotation.RequiresApi
 import androidx.core.app.NotificationCompat
 import androidx.lifecycle.LifecycleService
 import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Observer
 import com.dayo.runningapp.Other.Constants.ACTION_PAUSE_SERVICE
 import com.dayo.runningapp.Other.Constants.ACTION_SHOW_TRACKING_FRAGMENT
 import com.dayo.runningapp.Other.Constants.ACTION_START_OR_RESUME_SERVICE
 import com.dayo.runningapp.Other.Constants.ACTION_STOP_SERVICE
+import com.dayo.runningapp.Other.Constants.FASTEST_LOCATION_INTERVAL
+import com.dayo.runningapp.Other.Constants.LOCATION_UPDATE_INTERVAL
 import com.dayo.runningapp.Other.Constants.NOTIFICATION_CHANNEL_ID
 import com.dayo.runningapp.Other.Constants.NOTIFICATION_CHANNEL_NAME
 import com.dayo.runningapp.Other.Constants.NOTIFICATION_ID
+import com.dayo.runningapp.Other.TrackinUtility
 import com.dayo.runningapp.R
 import com.dayo.runningapp.ui.MainActivity
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationCallback
+import com.google.android.gms.location.LocationRequest.PRIORITY_HIGH_ACCURACY
+import com.google.android.gms.location.LocationResult
 import com.google.android.gms.maps.model.LatLng
 import timber.log.Timber
 
@@ -31,6 +42,11 @@ class TrackingService : LifecycleService() {
 
     var isFirstRun = true
 
+    lateinit var fusedLocationProviderClient: FusedLocationProviderClient
+
+    //companion objects in Kotlin are equivalent to static references in Java
+    //Java: public static string Kotlin: companion object
+    //It is a singleton instance of an actual object
     companion object{
         val isTracking = MutableLiveData<Boolean>()
         val pathPoints = MutableLiveData<Polylines>()
@@ -39,6 +55,16 @@ class TrackingService : LifecycleService() {
     private fun postInitialValues(){
         isTracking.postValue(false)
         pathPoints.postValue(mutableListOf())
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        postInitialValues()
+        fusedLocationProviderClient = FusedLocationProviderClient(this)
+
+        isTracking.observe(this, Observer {
+            updateLocationTracking(it)
+        })
     }
 
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
@@ -64,7 +90,58 @@ class TrackingService : LifecycleService() {
         return super.onStartCommand(intent, flags, startId)
     }
 
+    private fun updateLocationTracking(isTracking: Boolean){
+        if(isTracking){
+            if(TrackinUtility.hasLocationPermissions(this)){
+                val locationRequest = com.google.android.gms.location.LocationRequest().apply {
+                    interval = LOCATION_UPDATE_INTERVAL
+                    fastestInterval = FASTEST_LOCATION_INTERVAL
+                    priority = PRIORITY_HIGH_ACCURACY
+                }
+                fusedLocationProviderClient.requestLocationUpdates(
+                    locationRequest,
+                    locationCallback,
+                    Looper.getMainLooper()
+                )
+            }
+            else{
+                fusedLocationProviderClient.removeLocationUpdates(locationCallback)
+            }
+        }
+    }
+
+    val locationCallback = object : LocationCallback(){
+        override fun onLocationResult(result: LocationResult?) {
+            super.onLocationResult(result)
+            if(isTracking.value!!){
+                result?.locations?.let { locations->
+                    for(location in locations){
+                        addPathPoints(location)
+                        Timber.d("NEW LOCATION: ${location.longitude}, ${location.latitude}")
+                    }
+                }
+            }
+        }
+    }
+
+    private fun addPathPoints(location: Location){
+        location.let {
+            val position = LatLng(location.latitude, location.longitude)
+            pathPoints.value?.apply {
+                last().add(position)
+                pathPoints.postValue(this)
+            }
+        }
+    }
+
+    private fun addEmptyPolyline() = pathPoints.value?.apply {
+        add(mutableListOf())
+        pathPoints.postValue(this)
+    } ?: pathPoints.postValue(mutableListOf(mutableListOf()))
+
     private fun startForegroundService(){
+        addEmptyPolyline()
+        isTracking.postValue(true)
         val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
 
         if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
@@ -101,8 +178,4 @@ class TrackingService : LifecycleService() {
         notificationManager.createNotificationChannel(channel)
     }
 
-    private fun addEmptyPolyline() = pathPoints.value?.apply {
-        add(mutableListOf())
-        pathPoints.postValue(this)
-    }
 }
